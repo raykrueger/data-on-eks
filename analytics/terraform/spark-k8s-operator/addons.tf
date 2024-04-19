c9419ec2d1180dfe8fe21fb927e8f28c8a5fbca0
Ray Krueger <raykrueger@gmail.com>
2024-04-19 11:04:08 -0300

    Add s3-csi-serviceaccount
    Add S3 Mountpoint CSI addon

--- analytics/terraform/spark-k8s-operator/addons.tf
+++ analytics/terraform/spark-k8s-operator/addons.tf
@@ -15,6 +15,26 @@ module "ebs_csi_driver_irsa" {
   tags = local.tags
 }
 
+module "s3_csi_driver_irsa" {
+  source           = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version          = "~> 5.34"
+  role_name_prefix = format("%s-%s-", local.name, "s3-csi-driver")
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:s3-csi-controller"]
+    }
+  }
+  tags = local.tags
+}
+
+resource "aws_eks_addon" "s3_mountpoint_csi" {
+  cluster_name                = module.eks.cluster_name
+  addon_name                  = "aws-mountpoint-s3-csi-driver"
+  resolve_conflicts_on_update = "PRESERVE"
+  service_account_role_arn    = module.s3_csi_driver_irsa.iam_role_arn
+}
+
 #---------------------------------------------------------------
 # EKS Blueprints Addons
 #---------------------------------------------------------------
